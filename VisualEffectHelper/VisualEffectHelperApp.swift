a243211d10e6dd3b1aa431b2751e56d7470bed02
ash <as_supp@icloud.com>
2024-12-13 17:05:52 +0100

    Initial Commit

---
+++ VisualEffectHelper/VisualEffectHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualEffectHelperApp.swift
+//  VisualEffectHelper
+//
+//  Created by ash on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct VisualEffectHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisualEffectHelperTests/VisualEffectHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisualEffectHelperTests.swift
+//  VisualEffectHelperTests
+//
+//  Created by ash on 12/13/24.
+//
+
+import Testing
+@testable import VisualEffectHelper
+
+struct VisualEffectHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
