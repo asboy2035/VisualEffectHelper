f4ea92210903370aba02a4ac8b3ec83b61b85e4c
ash <as_supp@icloud.com>
2024-12-13 18:02:57 +0100

    update app to work on devices on macOS 11.5+

--- VisualEffectHelper/VisualEffectHelperApp.swift
+++ VisualEffectHelper/VisualEffectHelperApp.swift
@@ -14,6 +14,5 @@ struct VisualEffectHelperApp: App {
             ResponsiveGridView()
         }
         .windowStyle(HiddenTitleBarWindowStyle())
-        .windowResizability(.contentSize)
     }
 }
